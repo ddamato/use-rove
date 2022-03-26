3e9c6a38cf7685d3ae220d6f690377e3d97e0e2d
Donnie D'Amato <fauxserious@gmail.com>
2022-03-26 15:08:03 -0400

    Formatting

--- test/index.spec.js
+++ test/index.spec.js
@@ -39,72 +39,72 @@ describe(useRove.name, function () {
   describe('interactions', function () {
 
     it('should set focus by horizontal arrow keys', function () {
-      const { getByText } = render(<List />)
+      const { getByText } = render(<List />);
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowLeft}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{arrowLeft}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowLeft}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowLeft}');
+      expect(getByText('second')).toHaveFocus();
     });
 
     it('should set focus by vertical arrow keys', function () {
-      const { getByText } = render(<List />)
+      const { getByText } = render(<List />);
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowUp}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{arrowUp}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowDown}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowDown}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowDown}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowDown}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowDown}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowDown}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowUp}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowUp}');
+      expect(getByText('second')).toHaveFocus();
     });
 
     it('should restrict focus by arrow key orientation', function () {
-      const { getByText } = render(<List orientation='horizontal' />)
+      const { getByText } = render(<List orientation='horizontal' />);
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowUp}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{arrowUp}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowDown}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{arrowDown}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowDown}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowDown}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowUp}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowUp}');
+      expect(getByText('second')).toHaveFocus();
     });
 
     it('restore focus when returning', function () {
@@ -115,79 +115,79 @@ describe(useRove.name, function () {
         </>
       );
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.tab()
-      expect(getByRole('button')).toHaveFocus()
+      userEvent.tab();
+      expect(getByRole('button')).toHaveFocus();
 
       userEvent.tab({ shift: true })
-      expect(getByText('second')).toHaveFocus()
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('third')).toHaveFocus();
     });
 
     it('should focus by click/tap', function () {
-      const { getByText } = render(<List />)
+      const { getByText } = render(<List />);
 
       const clickTarget = getByText('second')
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.click(clickTarget)
-      expect(clickTarget).toHaveFocus()
+      userEvent.click(clickTarget);
+      expect(clickTarget).toHaveFocus();
 
-      userEvent.tab({ shift: true })
-      userEvent.tab()
-      expect(clickTarget).toHaveFocus()
+      userEvent.tab({ shift: true });
+      userEvent.tab();
+      expect(clickTarget).toHaveFocus();
     });
 
     it('should jump to first and last', function () {
-      const { getByText } = render(<List start='second' />)
+      const { getByText } = render(<List start='second' />);
 
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('second')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{end}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{end}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{home}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{home}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('second')).toHaveFocus();
     });
 
     it('should flip horizontally when RTL', function () {
       const { getByText } = render(<List rtl/>);
 
       // Remember all directions are inverted to reflect RTL behavior
-      expect(document.body).toHaveFocus()
-      userEvent.tab()
-      expect(getByText('first')).toHaveFocus()
+      expect(document.body).toHaveFocus();
+      userEvent.tab();
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('first')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('first')).toHaveFocus();
 
-      userEvent.keyboard('{arrowLeft}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowLeft}');
+      expect(getByText('second')).toHaveFocus();
 
-      userEvent.keyboard('{arrowLeft}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowLeft}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowLeft}')
-      expect(getByText('third')).toHaveFocus()
+      userEvent.keyboard('{arrowLeft}');
+      expect(getByText('third')).toHaveFocus();
 
-      userEvent.keyboard('{arrowRight}')
-      expect(getByText('second')).toHaveFocus()
+      userEvent.keyboard('{arrowRight}');
+      expect(getByText('second')).toHaveFocus();
     });
   });
 });
